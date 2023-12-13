f7911665f93829302ae4841913fa53c9fe276813
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-12-13 10:50:41 +0100

    comment

---
+++ advent1.py
@@ -0,0 +1,5 @@
+#1 loop each line.
+# find first and last nubmer in the line.
+# glue them togehter 1 ,3 = 13 
+#special case if it is only one number then it is like 7 -> 77
+#  the take addition of each line and get a final result.
