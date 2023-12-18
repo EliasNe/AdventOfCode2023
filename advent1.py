5075b1cb06e0e981d936bc8f5b2a3327b82182a4
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-12-18 15:34:31 +0100

    updated problem1

--- advent1.py
+++ advent1.py
@@ -3,3 +3,45 @@
 # glue them togehter 1 ,3 = 13 
 #special case if it is only one number then it is like 7 -> 77
 #  the take addition of each line and get a final result.
+
+
+import re
+
+
+txtFILE = "./inputAdvent1.txt"
+
+def firsAndTwolastnumber(txFILE):
+    sum = 0 
+    with open(txtFILE, 'r') as file:
+        
+        for line in file:
+            result = re.sub(r'\D', '',line) 
+            glueDigits = "".join([result[0],result[-1]])
+            sum += int(glueDigits)
+    return sum
+       
+        
+         
+            
+            
+                
+print(firsAndTwolastnumber(txtFILE))
+
+
+
+            
+                
+            
+                    
+                
+                    
+                
+                    
+                
+            
+          
+
+
+    
+    
+    
